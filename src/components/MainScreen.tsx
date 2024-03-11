7e9f95e295054e2ebae03efb16563c02b4249576
Bohdan <L.O.T.Future@gmail.com>
2024-03-11 17:41:08 +0200

    added info and some css

--- src/components/MainScreen.tsx
+++ src/components/MainScreen.tsx
@@ -1,24 +1,183 @@
 import { FunctionComponent, JSX } from 'react';
 import styled from "styled-components";
 
-const ComponentContainer = styled.div`
- font-family: 'Josefin Sans', sans-serif;
- font-size: 16px;
- color: #28292C;
- display: flex;
- flex-direction: column;
- align-items: center;
- user-select: none;
+import { jobplaces, projects, skills } from '../jobplaces';
+
+
+
+const AppContainer = styled.div`
+  font-family: 'Josefin Sans', sans-serif;
+  font-size: 20px;
+  background-color: #242424;
+  color: #ebebeb;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  user-select: none;
 `;
 
-interface MainScreenProps {  
-}
-const MainScreen: FunctionComponent<MainScreenProps> = (): JSX.Element => {
+const Header = styled.div`
+  display: flex;
+  position: fixed;
+`;
+
+const PhotoSection = styled.div`
+  display: flex;
+  justify-content: center;
+  position: relative;
+  margin: 5% 15% 0 15%;
+  width: 70%;
+  img {
+    width: 30%;
+  }
+  h1 {
+    font-size: 70px;
+    top: 50%;
+    position: absolute;
+  }
+`;
+const AboutSection = styled.div`
+  display: flex;
+  flex-direction: column;
+  position: relative;
+  margin: 5% 15% 0 15%;
+  width: 70%;
+  h2 {
+    color: #E5B80B;
+    font-size: 40px;
+  }
+`;
+const ContainerV = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+const ContainerH = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
+const SkillsSection = styled.div`
+  display: flex;
+  flex-direction: column;
+  position: relative;
+  margin: 5% 15% 0 15%;
+  width: 70%;
+  h2 {
+    color: #E5B80B;
+    font-size: 40px;
+  }
+`;
+const Skill = styled.div`
+  display: flex;
+  padding: 5px 10px;
+  margin: 5px 10px 0 0;
+  background-color: #E5B80B;
+  color: black;
+`;
+const PrevJobsSection = styled.div`
+  display: flex;
+  flex-direction: column;
+  position: relative;
+  margin: 5% 15% 0 15%;
+  width: 70%;
+  h2 {
+    color: #E5B80B;
+    font-size: 40px;
+  }
+`;
+const JobPlace = styled.div`
+  display: flex;
+  flex-direction: column;
+  h2 {
+    color: #ebebeb;
+    font-size: 30px;
+  }
+`;
+const ProjectsSection = styled.div`
+  display: flex;
+  flex-direction: column;
+  position: relative;
+  margin: 5% 15% 0 15%;
+  width: 70%;
+  h2 {
+    color: #E5B80B;
+    font-size: 40px;
+  }
+`;
+const Project = styled.div`
+  display: flex;
+`;
+const Footer = styled.div`
+  display: flex;
+`;
+
+
+const MainScreen: FunctionComponent = (): JSX.Element => {
+  
 
     return (
-        <ComponentContainer>
-        
-        </ComponentContainer>
+        <AppContainer>
+          <Header>Header</Header>
+          
+          <PhotoSection>
+            <img src="img/BohdanP.png" alt="bohdan" />
+            <h1>Javascript Developer</h1>
+          </PhotoSection>
+          <AboutSection>
+            <h2>A little bit about me:</h2>
+            <ContainerH>
+              <ContainerV style={{width: '60%'}}>
+                <p>Hello there!</p>
+                <p>Name is Bohdan and I am sociable, curious and positive person. And actually, a web developer with 2 years of experience in JavaScript and React. My earlier background spans across various domains. Transitioning from roles in project management and system administration to a freelance career in graphic design and website markup underscores my versatility. I'm deeply committed to continuous learning, as evidenced by my completion of numerous online courses in web development and React.js.</p>
+                <p>Now I have theoretical knowledge, along with pet project, in Svelte. So I am eager to use them in real scenarios.</p>
+                <p></p>
+              </ContainerV>
+              <img src="" alt="cool something" />
+            </ContainerH>
+          </AboutSection>
+          <SkillsSection>
+            <h2>My skills:</h2>
+            <ContainerH>
+              {skills.map((skill) => {
+                return  (
+                  <Skill>{skill}</Skill>
+                );
+              })}
+            </ContainerH>
+          </SkillsSection>
+          
+          <PrevJobsSection>
+            <h2>My job places:</h2>
+            <ContainerH>
+              {jobplaces.map((jobplace) => {
+                return  (
+                  <JobPlace>
+                    <h2>{jobplace.name}</h2>
+                    <h3>{jobplace.time}</h3>
+                    <h4>{jobplace.position}</h4>
+                    <p>{jobplace.description}</p>
+                  </JobPlace>
+                );
+              })}
+            </ContainerH>
+          </PrevJobsSection>
+          <ProjectsSection>
+            <h2>My projects:</h2>
+            <ContainerV>
+              {projects.map((project) => {
+                return  (
+                  <Project>
+                    <h3>{project.name}</h3>
+                    <img src={project.img} alt=""/>
+                    <p>{project.description}</p>
+                    <h4>{project.link}</h4>
+                  </Project>
+                );
+              })}
+            </ContainerV>
+          </ProjectsSection>
+          
+          <Footer></Footer>
+        </AppContainer>
       );
 };
 
---
+++ src/jobplaces.ts
@@ -0,0 +1,34 @@
+export const jobplaces = [
+    {name:"Freelance developer", time:"Mar 2023 - now", position:"web developer", description:`
+    It is hard to find a job in Ukraine during war. That's why I worked only on some small website developing projects on freelance and for friends. I am not including this time in my total work experience.
+    `}, 
+    {name:"Sprakkraft - Brainglass,", time:"May 2021 - Feb 2023", position:"web developer & app designer", description:`
+    My last job was at a Swedish company where I worked remotely as a frontend developer and also simultaneously as an
+    web-designer on the projects we were working on.
+    Here you can see my designs for the app “Reading Coach”, all of them were also developed by me
+    https://www.figma.com/file/5CCFGqSQnm7Fc8Yiofc3yJ/Untitled?type=design&node-id=0%3A1&mode=design&t=JstBjvFsBxBOPwyn-1
+    
+    Additional to that I was responsible for maintenance ofwebsites: sprakkraft.se, sprakkraft.org
+    Also on this job we had outsource contracts with “UR Play”and “YLE”, mostly maintenance of their websites and adding new features to video players.
+    `}, 
+    {name:"Jet-up, Khmelnitskiy", time:"Sep 2020 - Nov 2020", position:"junior react-developer", description:`After I learned React on web-courses, I started my
+    developer path in a local company, but I wasn't there for a long time because of Covid pandemic and the traffic crisis that appeared because of that.
+    `},
+    {name:"Upwork, freelance", time:"Aug 2017 - Jan 2020", position:"2d artist, web-designer & markup-developer", description:`Long time I was working as a freelancer. I started as an artist
+    for pixel-art games and web-designer. But later I learned html/css and started to take projects for developing templates and simple landing web-pages.
+    `},
+    {name:"Xedex", time:"Feb 2015 - May 2016", position:"project-manager", description:`A small local outsource IT company. I was a projects-manager there, a link between clients and team. A lot of communication and teamwork.
+    `}
+];
+
+export const projects = [
+    {img:"", name:`huge website portfolio`, description:`Website for a ui/ux designer, with possibility to add new works in a simple way. Clean, with some animations. Design was provided by her.`, link:``},
+    {img:"", name:`part of the web-app`, description:`Designed and implemented by myself. Now it is the part of the web app - "Lingoal"`, link:""},
+    {img:"", name:`calendar app`, description:`Pet project based on test-task. Has many possibilities, just click and see by yourself.`, link:``},
+    {img:"", name:``, description:``, link:``},
+];
+
+export const skills = [
+    "HTML", "CSS", "Javascript", "Typescript", "React.js", "Styled-components",
+    "Figma", "Adobe Photoshop"
+];
